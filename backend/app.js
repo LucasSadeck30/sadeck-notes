d85b81da2f89256fcb2121b81532245ed7c2ce2f
LucasSadeck30 <lucasdevsadeck@gmail.com>
2026-03-12 08:56:44 -0300

    Criação de funcionalidade de user Lost Password3

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,17 @@ let usersRouter = require('./app/routes/users.js');
 let notesRouter = require('./app/routes/notes.js');
 
 var app = express();
+// DEPOIS
+app.use(cors({
+  origin: [
+      'http://localhost:3000',
+    'http://localhost:3001', // desenvolvimento
+    'https://sadeck-notes.vercel.app', // produção (substitua pela sua URL)
+    'https://sadeck-notes-*.vercel.app',
+    'https://sadeck-notes-27fc.vercel.app' // previews
+  ],
+  credentials: true
+}))
 
 
 
@@ -18,16 +29,6 @@ app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
 
-// DEPOIS
-app.use(cors({
-  origin: [
-      'http://localhost:3000',
-    'http://localhost:3001', // desenvolvimento
-    'https://sadeck-notes.vercel.app', // produção (substitua pela sua URL)
-    'https://sadeck-notes-*.vercel.app' // previews
-  ],
-  credentials: true
-}))
 
 
 app.use(express.static(path.join(__dirname, 'public')));
